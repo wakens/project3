57cc6948bcb493a40e269b4c7ad9f9807820a102
wakens <65771520+wakens@users.noreply.github.com>
2020-05-29 14:48:04 -0400

    Update JOPListener.java

--- JOPListener.java
+++ JOPListener.java
@@ -3,12 +3,9 @@ import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 public class JOPListener implements ActionListener
 {
+    // This method reacts when the JOptionPane is clicked
     @Override
     public void actionPerformed(final ActionEvent e) {
         JOptionPane.showMessageDialog(null, "You clicked me!");
